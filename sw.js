712ed3db5c27e1ba48395cf508cfa15d0b943ff6
Jereme Allen <jereme.allen@wheniwork.com>
2017-07-16 20:15:59 -0500

    Initial commit

---
+++ sw.js
@@ -0,0 +1,5 @@
+'use strict';
+importScripts('./sw-toolbox.js'); 
+toolbox.precache(['index.html','/css/freelancer.css']); 
+toolbox.router.get('/img/*', toolbox.cacheFirst); 
+toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
